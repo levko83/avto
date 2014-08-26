f9c693a9777ecc72afb18f535df043e247a8b604
Mevis <Evgentiy83@mail.ru>
2014-08-26 14:52:38 +0300

    Change config console application

--- protected/config/console.php
+++ protected/config/console.php
@@ -9,8 +9,8 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Console Application',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+//	// preloading 'log' component
+//	'preload'=>array('log'),
 
 	// application components
 	'components'=>array(
@@ -24,14 +24,14 @@ return array(
             'password' => $_db_settings->main_db_pass,
             'charset' => 'utf8',
 		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-			),
-		),
+//		'log'=>array(
+//			'class'=>'CLogRouter',
+//			'routes'=>array(
+//				array(
+//					'class'=>'CFileLogRoute',
+//					'levels'=>'error, warning',
+//				),
+//			),
+//		),
 	),
 );
