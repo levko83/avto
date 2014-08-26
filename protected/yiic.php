79754e7fd44864795c09f535d590a261b2c868ab
Mevis <Evgentiy83@mail.ru>
2014-08-26 15:55:08 +0300

    Set memory_limit = 512M for console application

--- protected/yiic.php
+++ protected/yiic.php
@@ -2,6 +2,8 @@
 
 // change the following paths if necessary
 
+ini_set("memory_limit", "512M");
+
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 $yiic=dirname(__FILE__).'/../framework/yiic.php';
