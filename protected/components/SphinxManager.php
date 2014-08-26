9db96ccf74ff39ec01e70c8e8b33752b52a571e7
Mevis <Evgentiy83@mail.ru>
2014-08-27 01:21:35 +0300

    Change SphinxManager fo reindex

--- protected/components/SphinxManager.php
+++ protected/components/SphinxManager.php
@@ -10,15 +10,22 @@
 class SphinxManager {
 
     public static function reindex($indexName = null){
+        $path = Yii::getPathOfAlias("application.shell");
         if($indexName){
-            return shell_exec("indexer {$indexName} --rotate");
+            return shell_exec("sh {$path}/sphinx_index.sh {$indexName}");
         }else{
-            return shell_exec("indexer --all --rotate");
+            return shell_exec("sh {$path}/sphinx_index.sh");
         }
+//        if($indexName){
+//            return shell_exec("sudo -u www-data indexer {$indexName} --rotate");
+//        }else{
+//            return shell_exec("sudo -u www-data indexer --all --rotate");
+//        }
     }
 
     public static function restart(){
-        return shell_exec("sudo service sphinxsearch restart");
+        $result = shell_exec("sudo service sphinxsearch restart");
+        return $result;
     }
 
 }
