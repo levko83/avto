f8ecdca3b66f43d6146fcef33109392453a9b37a
Mevis <Evgentiy83@mail.ru>
2014-08-31 20:20:22 +0300

    Change image directory permissions after autopull

--- autopull.php
+++ autopull.php
@@ -2,7 +2,7 @@
     error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
     $date_now = date("d-m-Y G:i:s");
     if(isset($_GET["key"]) and $_GET["key"] == "38d1d6b862a552198acae32977b283ce"){
-        $result = shell_exec("git checkout -- * && git fetch && git merge remotes/origin/master");
+        $result = shell_exec("git checkout -- * && git fetch && git merge remotes/origin/master && chmod -R 777 ./images");
         $result = "{$date_now}\n{$result}\n\n";
         file_put_contents(__DIR__."/protected/runtime/autopull.log", $result, FILE_APPEND);
     }else{
