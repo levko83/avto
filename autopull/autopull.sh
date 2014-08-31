c2f64530b40336d2eb799b8d8309e9c18db2d1e1
Mevis <Evgentiy83@mail.ru>
2014-08-31 17:45:47 +0300

    Change algorithm autopull and logging POST data

--- autopull/autopull.sh
+++ autopull/autopull.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cd ..
-sudo -u ponzps26 git checkout -- *
-sudo -u ponzps26 git fetch
-sudo -u ponzps26 git merge remotes/origin/master
+cd .. && git checkout -- * && git fetch && git merge remotes/origin/master
 
 
 
--- autopull/index.php
+++ autopull/index.php
@@ -1,6 +1,7 @@
 <?php
     error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-    $result = shell_exec("sh ".__DIR__."/autopull.sh");
+    $result = shell_exec("cd .. && git checkout -- * && git fetch && git merge remotes/origin/master");
     $date_now = date("d-m-Y G:i:s");
-    $result = "{$date_now}\n{$result}\n\n";
+    $post_data = "Post: ".print_r($_POST, true);
+    $result = "{$date_now}\n{$result}\n\n{$post_data}";
     file_put_contents(__DIR__."/../protected/runtime/autopull.log", $result, FILE_APPEND);
