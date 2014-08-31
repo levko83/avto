204f43d40b80d5dd2f88aaf2fd9ed3e83ea78d66
Mevis <Evgentiy83@mail.ru>
2014-08-31 18:14:19 +0300

    Redirect with unsuccessfull autopull on main site page

--- autopull.php
+++ autopull.php
@@ -1,11 +1,15 @@
 <?php
     error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+    $date_now = date("d-m-Y G:i:s");
     if(isset($_GET["key"]) and $_GET["key"] == "38d1d6b862a552198acae32977b283ce"){
         $result = shell_exec("git checkout -- * && git fetch && git merge remotes/origin/master");
+        $result = "{$date_now}\n{$result}\n\n";
+        file_put_contents(__DIR__."/protected/runtime/autopull.log", $result, FILE_APPEND);
     }else{
         $ip = $_SERVER['REMOTE_ADDR'];
         $result = "Unsuccessful pull form {$ip}";
+        $result = "{$date_now}\n{$result}\n\n";
+        file_put_contents(__DIR__."/protected/runtime/autopull.log", $result, FILE_APPEND);
+        header("Location: /");
+        exit;
     }
-    $date_now = date("d-m-Y G:i:s");
-    $result = "{$date_now}\n{$result}\n\n";
-    file_put_contents(__DIR__."/protected/runtime/autopull.log", $result, FILE_APPEND);
