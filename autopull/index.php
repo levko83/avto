d56464195d1ec176591d5aeadd9b5065df91b549
Mevis <Evgentiy83@mail.ru>
2014-08-31 17:09:10 +0300

    Add logging with autopull

--- autopull/index.php
+++ autopull/index.php
@@ -1,4 +1,6 @@
 <?php
     error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
     $result = shell_exec("sh ".__DIR__."/autopull.sh");
-    echo $result;
+    $date_now = date("d-m-Y G:i:s");
+    $result = "{$date_now}\n{$result}\n\n";
+    file_put_contents(__DIR__."/../protected/runtime/autopull.log", $result, FILE_APPEND);
