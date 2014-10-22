4e3bebf41285fae3f4b853a01686d9ee4209967e
Mevis <Evgentiy83@mail.ru>
2014-10-22 15:41:37 +0300

    Очистка файлов сессий дамперов

--- int_comp/sxd/ses.php
+++ int_comp/sxd/ses.php
@@ -1,31 +1,3 @@
 <?php
-$SES = array (
-  '66fb46279d877490144da41c7c1bcfcd' => 
-  array (
-    'cfg' => 
-    array (
-      'charsets' => 'cp1251 utf8 latin1',
-      'lang' => 'ru',
-      'time_web' => '600',
-      'time_cron' => '600',
-      'backup_path' => 'backup/',
-      'backup_url' => 'backup/',
-      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
-      'globstat' => 0,
-      'my_host' => 'localhost',
-      'my_port' => 3306,
-      'my_user' => 'root',
-      'my_pass' => 'root',
-      'my_comp' => 0,
-      'my_db' => 'avto_parser',
-      'auth' => 'admin mysql cfg',
-      'user' => '',
-      'pass' => '',
-      'confirm' => '6',
-      'exitURL' => './',
-    ),
-    'time' => 1413892991,
-    'lng' => 'ru',
-  ),
-);
+$SES = array();
 ?>
